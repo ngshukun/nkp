263c6432f1042e95fe50eca46fcab41ccad78947
shukun <ngshukun@gmail.com>
2025-11-07 16:07:28 +0800

    include cert generation for baremetal setup

--- baremetal-setup.sh
+++ baremetal-setup.sh
@@ -32,6 +32,76 @@ k version           # to verify the kubectl version
 # generate ssh pub key so that we can ssh to worker nodes
 ssh-keygen 
 
+# Create ca-chain  cert
+mkdir -p certs
+cd certs
+
+COUNTRY="SG"
+ORG="YourOrg Ltd"
+ROOT_CN="YourOrg Root CA"
+ICA_CN="YourOrg Intermediate CA"
+ROOT_DAYS=3650                             # ~10 years
+ICA_DAYS=3650
+SERVER_DAYS=825                            # ~27 months (common max for public TLS)
+# For v3_server.ext
+SERVER_CN="baremetal.ntnxlab.local"   # CN not used for matching, but keep it tidy
+SERVER_HOST1="baremetal.ntnxlab.local"
+SERVER_IP1="10.129.42.20"
+# SERVER_HOST2="*.ntnxlab.local"
+# SERVER_IP2="10.129.42.94"
+
+
+# generate root ca cert
+cat > v3_ca.ext <<'EOF'
+[ req ]
+x509_extensions    = v3_ca
+prompt             = no
+
+[ v3_ca ]
+basicConstraints = critical, CA:true
+keyUsage = critical, keyCertSign, cRLSign
+subjectKeyIdentifier = hash
+authorityKeyIdentifier = keyid:always,issuer
+EOF
+
+openssl genrsa -out root.key 4096
+chmod 600 root.key
+
+openssl req -new -x509 -sha256 -days "$ROOT_DAYS" \
+  -key root.key \
+  -subj "/C=$COUNTRY/O=$ORG/CN=$ROOT_CN" \
+  -config v3_ca.ext -extensions v3_ca \
+  -out root.crt
+
+#create Intermediate cert
+openssl genrsa -out ica.key 4096
+chmod 600 ica.key
+
+openssl req -new -sha256 \
+  -key ica.key \
+  -subj "/C=$COUNTRY/O=$ORG/CN=$ICA_CN" \
+  -out ica.csr
+
+cat > v3_ica.ext <<'EOF'
+[ req ]
+x509_extensions    = v3_ica
+prompt             = no
+
+[ v3_ica ]
+basicConstraints = critical, CA:true
+keyUsage = critical, keyCertSign, cRLSign
+subjectKeyIdentifier = hash
+authorityKeyIdentifier = keyid:always,issuer
+EOF
+
+openssl x509 -req -sha256 -days "$ICA_DAYS" \
+  -in ica.csr \
+  -CA root.crt -CAkey root.key -CAcreateserial \
+  -extfile v3_ica.ext -extensions v3_ica \
+  -out ica.crt
+
+cat ica.crt root.crt > ca-chain.crt
+
 
 # in internet connect laptop, run the following command
 cd nkp-v2.16.0/kib
