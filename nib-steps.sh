c1ec26778522053b1251c609cb4a96340506aef5
shukun <ngshukun@gmail.com>
2026-02-02 11:28:10 +0800

    update example for NIB steps

--- nib-steps.sh
+++ nib-steps.sh
@@ -11,3 +11,25 @@ rhel-8.10 \
 --subnet <subnet_name_or_UUID_associated_with_Prism_Element> \
 --source-image <base_image_name_or_UUID_or_URL> \
 PKR_VAR_disk_size_gb=<size of vm> # if vm disk is more then 20Gib
+
+# example
+export NUTANIX_ENDPOINT=https://10.21.102.154
+export NUTANIX_CLUSTER=kestrel01-2
+export NUTANIX_USER=admin
+export NUTANIX_PASSWORD=nx2Tech094!
+export SUBNET=NKP_Network
+export BASE_IMAGE="nkp-2.17-jumphost-noble-server-cloudimg-amd64.img"
+export OS=ubuntu-24.04
+export ARTIFACTS_DIRECTORY_FLAG="--artifacts-directory=/home/ubuntu/nkp-v2.17.0/image-artifacts"
+export BUNDLE_FLAG="bundle /home/ubuntu/nkp-v2.17.0/container-images/konvoy-image-bundle-v2.17.0.tar"
+
+
+PKR_VAR_disk_size_gb=120 nkp create image nutanix $OS \
+    --endpoint "${NUTANIX_ENDPOINT}" \
+    --cluster "${NUTANIX_CLUSTER}" \
+    --subnet "${SUBNET}" \
+    --source-image "${BASE_IMAGE}" \
+    "${ARTIFACTS_DIRECTORY_FLAG:-""}" \
+    --${BUNDLE_FLAG:-""} \
+    --insecure \
+    -v6
