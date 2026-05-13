218d839e29c456df2db3808a19f7769590a86803
ngshukun <ngshukun@gmail.com>
2026-05-13 16:59:07 +0800

    update new steps

---
+++ block internet and unblock.sh
@@ -0,0 +1,9 @@
+# in rx, if you need to mimit airgap, use the following
+
+vi ~/.bashrc
+
+# Toggle Air-Gap
+alias unblock-internet='sudo iptables -D OUTPUT -o ens3 ! -d 10.161.83.0/24 -m conntrack --ctstate NEW -j REJECT 2>/dev/null'
+alias block-internet='sudo iptables -A OUTPUT -o ens3 ! -d 10.161.83.0/24 -m conntrack --ctstate NEW -j REJECT'
+
+source ~/.bashrc
---
+++ rabbitMQ.sh
@@ -0,0 +1,3 @@
+docker pull bitnamilegacy/rabbitmq-cluster-operator:2.16.1-debian-12-r0
+docker pull bitnamilegacy/rmq-messaging-topology-operator:1.17.4-debian-12-r0
+docker pull bitnamilegacy/rabbitmq:4.1.3-debian-12-r1
